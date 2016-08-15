e15e2cd53ee52cfabe8a0709291e821f53e80a17
Noah Gibbs <the.codefolio.guy@gmail.com>
2016-08-15 09:22:55 -0700

    Add manual-return-value example

---
+++ examples/measured_sleep.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require "abprof"
+
+puts "ABProf example: sleep 0.1 seconds, manual return value"
+
+ABProf::ABWorker.iteration_with_return_value do
+  t1 = Time.now
+  sleep 0.001
+  (Time.now - t1)
+end
+
+ABProf::ABWorker.start
